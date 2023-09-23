c21256dc9c7cfa9f3091412520e06f198df3cd3d
Lê Quốc Huy <97956803+QuocHuy03@users.noreply.github.com>
2023-09-23 09:33:37 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,17 @@
 import { useContext, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Routes, useNavigate } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
 import { AppContext } from "./contexts/AppProviderContext";
 import { AppState } from "./store";
 import { AuthenticatedUser } from "./types";
-import Login from "./pages/AuthPage";
-import Home from "./pages/DashboardPage";
-import BrandPage from "./pages/BrandPage";
-import CategoryPage from "./pages/CategoryPage";
-import AddProduct from "./pages/ProductPage/Add";
-import ListProduct from "./pages/ProductPage/List";
-import EditProduct from "./pages/ProductPage/Edit";
-import ListOrder from "./pages/OrderPage";
-import BlogPage from "./pages/BlogPage";
-import CouponPage from "./pages/CouponPage";
-import UserPage from "./pages/UserPage";
-import CommentPage from "./pages/CommentPage";
-import NotFoundPage from "./pages/NotFoundPage";
 import { io } from "socket.io-client";
 import { v4 as uuidv4 } from "uuid";
 import { CUSTOM_ENV } from "./utils/custom.env";
 import { addNotification } from "./store/notification/actions";
-import PromotionPage from "./pages/PromotionPage";
-import PaymentPage from "./pages/PaymentPage";
-import BannerPage from "./pages/BannerPage";
-import CouponUserPage from "./pages/CouponUserPage";
+import getUserRoutes from "./routes";
 function App() {
+  const allowedRoles = ["ADMIN", "STAFF"];
   const { loadingBarProgress, setLoadingBarProgress } =
     useContext<any>(AppContext);
   const navigate = useNavigate();
@@ -150,7 +135,7 @@ function App() {
 
   useEffect(() => {
     if (user) {
-      if (user.role === "ADMIN" && user.verify == 1) {
+      if (allowedRoles.includes(user.role) && user.verify == 1) {
         navigate("/");
       } else {
         navigate("/auth");
@@ -168,109 +153,7 @@ function App() {
         waitingTime={700}
         onLoaderFinished={() => setLoadingBarProgress(0)}
       />
-      <Routes>
-        {user && user.role === "ADMIN" && user.verify == 1 ? (
-          <>
-            <Route
-              path="/"
-              element={<Home setLoadingBarProgress={setLoadingBarProgress} />}
-            />
-            <Route
-              path="/categories"
-              element={
-                <CategoryPage setLoadingBarProgress={setLoadingBarProgress} />
-              }
-            />
-            <Route
-              path="/brands"
-              element={
-                <BrandPage setLoadingBarProgress={setLoadingBarProgress} />
-              }
-            />
-            <Route
-              path="/products"
-              element={
-                <ListProduct setLoadingBarProgress={setLoadingBarProgress} />
-              }
-            />
-            <Route
-              path="/product/add"
-              element={
-                <AddProduct setLoadingBarProgress={setLoadingBarProgress} />
-              }
-            />
-            <Route
-              path="/product/edit/:slug"
-              element={
-                <EditProduct setLoadingBarProgress={setLoadingBarProgress} />
-              }
-            />
-            <Route
-              path="/orders"
-              element={
-                <ListOrder setLoadingBarProgress={setLoadingBarProgress} />
-              }
-            />
-            <Route
-              path="/banners"
-              element={
-                <BannerPage setLoadingBarProgress={setLoadingBarProgress} />
-              }
-            />
-            <Route
-              path="/blogs"
-              element={
-                <BlogPage setLoadingBarProgress={setLoadingBarProgress} />
-              }
-            />
-            <Route
-              path="/promotions"
-              element={
-                <PromotionPage setLoadingBarProgress={setLoadingBarProgress} />
-              }
-            />
-            <Route
-              path="/coupons"
-              element={
-                <CouponPage setLoadingBarProgress={setLoadingBarProgress} />
-              }
-            />
-            <Route
-              path="/discounts"
-              element={
-                <CouponUserPage setLoadingBarProgress={setLoadingBarProgress} />
-              }
-            />
-
-            <Route
-              path="/users"
-              element={
-                <UserPage setLoadingBarProgress={setLoadingBarProgress} />
-              }
-            />
-
-            <Route
-              path="/comments"
-              element={
-                <CommentPage setLoadingBarProgress={setLoadingBarProgress} />
-              }
-            />
-            <Route
-              path="/payments"
-              element={
-                <PaymentPage setLoadingBarProgress={setLoadingBarProgress} />
-              }
-            />
-
-            <Route path="*" element={<NotFoundPage />} />
-          </>
-        ) : (
-          <>
-            <Route path="/auth" element={<Login />} />
-            <Route path="*" element={<Login />} />
-          </>
-        )}
-      </Routes>
+      <Routes>{getUserRoutes(user, setLoadingBarProgress)}</Routes>
     </>
   );
 }
---
+++ src/routes/index.tsx
@@ -0,0 +1,187 @@
+import { Route } from "react-router-dom";
+import Home from "../pages/DashboardPage";
+import CategoryPage from "../pages/CategoryPage";
+import NotFoundPage from "../pages/NotFoundPage";
+import Login from "../pages/AuthPage";
+import PaymentPage from "../pages/PaymentPage";
+import BrandPage from "../pages/BrandPage";
+import ListProduct from "../pages/ProductPage/List";
+import EditProduct from "../pages/ProductPage/Edit";
+import AddProduct from "../pages/ProductPage/Add";
+import ListOrder from "../pages/OrderPage";
+import BannerPage from "../pages/BannerPage";
+import BlogPage from "../pages/BlogPage";
+import PromotionPage from "../pages/PromotionPage";
+import CouponPage from "../pages/CouponPage";
+import CouponUserPage from "../pages/CouponUserPage";
+import UserPage from "../pages/UserPage";
+import CommentPage from "../pages/CommentPage";
+
+export default function getUserRoutes(user: any, setLoadingBarProgress: any) {
+  if (user && user.role === "ADMIN" && user.verify == 1) {
+    return (
+      <>
+        <Route
+          path="/"
+          element={<Home setLoadingBarProgress={setLoadingBarProgress} />}
+        />
+        <Route
+          path="/categories"
+          element={
+            <CategoryPage setLoadingBarProgress={setLoadingBarProgress} />
+          }
+        />
+        <Route
+          path="/brands"
+          element={<BrandPage setLoadingBarProgress={setLoadingBarProgress} />}
+        />
+        <Route
+          path="/products"
+          element={
+            <ListProduct setLoadingBarProgress={setLoadingBarProgress} />
+          }
+        />
+        <Route
+          path="/product/add"
+          element={<AddProduct setLoadingBarProgress={setLoadingBarProgress} />}
+        />
+        <Route
+          path="/product/edit/:slug"
+          element={
+            <EditProduct setLoadingBarProgress={setLoadingBarProgress} />
+          }
+        />
+        <Route
+          path="/orders"
+          element={<ListOrder setLoadingBarProgress={setLoadingBarProgress} />}
+        />
+        <Route
+          path="/banners"
+          element={<BannerPage setLoadingBarProgress={setLoadingBarProgress} />}
+        />
+        <Route
+          path="/blogs"
+          element={<BlogPage setLoadingBarProgress={setLoadingBarProgress} />}
+        />
+        <Route
+          path="/promotions"
+          element={
+            <PromotionPage setLoadingBarProgress={setLoadingBarProgress} />
+          }
+        />
+        <Route
+          path="/coupons"
+          element={<CouponPage setLoadingBarProgress={setLoadingBarProgress} />}
+        />
+        <Route
+          path="/discounts"
+          element={
+            <CouponUserPage setLoadingBarProgress={setLoadingBarProgress} />
+          }
+        />
+
+        <Route
+          path="/users"
+          element={<UserPage setLoadingBarProgress={setLoadingBarProgress} />}
+        />
+
+        <Route
+          path="/comments"
+          element={
+            <CommentPage setLoadingBarProgress={setLoadingBarProgress} />
+          }
+        />
+        <Route
+          path="/payments"
+          element={
+            <PaymentPage setLoadingBarProgress={setLoadingBarProgress} />
+          }
+        />
+
+        <Route path="*" element={<NotFoundPage />} />
+      </>
+    );
+  } else if (user && user.role === "STAFF" && user.verify == 1) {
+    return (
+      <>
+        <Route
+          path="/"
+          element={<Home setLoadingBarProgress={setLoadingBarProgress} />}
+        />
+        <Route
+          path="/categories"
+          element={
+            <CategoryPage setLoadingBarProgress={setLoadingBarProgress} />
+          }
+        />
+        <Route
+          path="/brands"
+          element={<BrandPage setLoadingBarProgress={setLoadingBarProgress} />}
+        />
+        <Route
+          path="/products"
+          element={
+            <ListProduct setLoadingBarProgress={setLoadingBarProgress} />
+          }
+        />
+        <Route
+          path="/product/add"
+          element={<AddProduct setLoadingBarProgress={setLoadingBarProgress} />}
+        />
+        <Route
+          path="/product/edit/:slug"
+          element={
+            <EditProduct setLoadingBarProgress={setLoadingBarProgress} />
+          }
+        />
+        <Route
+          path="/orders"
+          element={<ListOrder setLoadingBarProgress={setLoadingBarProgress} />}
+        />
+        <Route
+          path="/banners"
+          element={<BannerPage setLoadingBarProgress={setLoadingBarProgress} />}
+        />
+        <Route
+          path="/blogs"
+          element={<BlogPage setLoadingBarProgress={setLoadingBarProgress} />}
+        />
+        <Route
+          path="/promotions"
+          element={
+            <PromotionPage setLoadingBarProgress={setLoadingBarProgress} />
+          }
+        />
+        <Route
+          path="/coupons"
+          element={<CouponPage setLoadingBarProgress={setLoadingBarProgress} />}
+        />
+        <Route
+          path="/discounts"
+          element={
+            <CouponUserPage setLoadingBarProgress={setLoadingBarProgress} />
+          }
+        />
+
+        <Route
+          path="/users"
+          element={<UserPage setLoadingBarProgress={setLoadingBarProgress} />}
+        />
+
+        <Route
+          path="/payments"
+          element={
+            <PaymentPage setLoadingBarProgress={setLoadingBarProgress} />
+          }
+        />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Route path="/auth" element={<Login />} />
+        <Route path="*" element={<Login />} />
+      </>
+    );
+  }
+}
