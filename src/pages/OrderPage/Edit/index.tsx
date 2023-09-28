22dcc256a54e1d929cae554c431cc7458d9c5408
Lê Quốc Huy <97956803+QuocHuy03@users.noreply.github.com>
2023-09-28 15:13:02 +0700

    Update index.tsx

--- src/pages/OrderPage/Edit/index.tsx
+++ src/pages/OrderPage/Edit/index.tsx
@@ -11,6 +11,7 @@ import { useSelector } from "react-redux";
 import { orderService } from "../../../services/order.service";
 import { paymentService } from "../../../services/payment.service";
 import { userService } from "../../../services/user.service";
+import Loading from "../../../components/Loading";
 
 type Props = {
   setLoadingBarProgress: any;
@@ -66,6 +67,7 @@ const EditOrder: React.FC<Props> = ({ setLoadingBarProgress }) => {
       retryDelay: 1000,
     }
   );
+  
   const { data: isPayment } = useQuery(
     ["payments"],
     () => paymentService.fetchAllPayments(),
@@ -141,193 +143,205 @@ const EditOrder: React.FC<Props> = ({ setLoadingBarProgress }) => {
         </Link>
       </div>
       <div className="pt-6">
-        <Form
-          form={form}
-          layout="vertical"
-          autoComplete="off"
-          onFinish={onFinish}
-        >
-          <Form.List name="products">
-            {(fields, { add, remove }) => (
-              <>
-                <div className="grid gap-6 md:grid-cols-2">
-                  {fields.map(({ key, name, fieldKey, ...restField }) => (
-                    <div key={key}>
-                      <Form.Item
-                        {...restField}
-                        label={`Product #${name}`}
-                        name={[name, "productID"]}
-                        fieldKey={`[${fieldKey}, "productID"]`}
-                        style={{
-                          marginBottom: 0,
-                        }}
-                        rules={[
-                          { required: true, message: "* Product is required" },
-                        ]}
-                      >
-                        <Select size={SIZEFORM} placeholder="Chọn Sản Phẩm">
-                          <Select.Option value="">
-                            Vui Lòng Chọn Sản Phẩm
-                          </Select.Option>
-                          {isProduct?.map((item: any) => (
-                            <Select.Option key={item._id} value={item._id}>
-                              {item.nameProduct}
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <Form
+            form={form}
+            layout="vertical"
+            autoComplete="off"
+            onFinish={onFinish}
+          >
+            <Form.List name="products">
+              {(fields, { add, remove }) => (
+                <>
+                  <div className="grid gap-6 md:grid-cols-2">
+                    {fields.map(({ key, name, fieldKey, ...restField }) => (
+                      <div key={key}>
+                        <Form.Item
+                          {...restField}
+                          label={`Product #${name}`}
+                          name={[name, "productID"]}
+                          fieldKey={`[${fieldKey}, "productID"]`}
+                          style={{
+                            marginBottom: 0,
+                          }}
+                          rules={[
+                            {
+                              required: true,
+                              message: "* Product is required",
+                            },
+                          ]}
+                        >
+                          <Select size={SIZEFORM} placeholder="Chọn Sản Phẩm">
+                            <Select.Option value="">
+                              Vui Lòng Chọn Sản Phẩm
                             </Select.Option>
-                          ))}
-                        </Select>
-                      </Form.Item>
+                            {isProduct?.map((item: any) => (
+                              <Select.Option key={item._id} value={item._id}>
+                                {item.nameProduct}
+                              </Select.Option>
+                            ))}
+                          </Select>
+                        </Form.Item>
 
-                      <Form.Item
-                        {...restField}
-                        label={`Color #${name}`}
-                        name={[name, "color"]}
-                        fieldKey={`[${fieldKey}, "color"]`}
-                        style={{
-                          marginBottom: 0,
-                        }}
-                        rules={[
-                          { required: true, message: "* Color is required" },
-                        ]}
-                      >
-                        <Select size={SIZEFORM} placeholder="Color">
-                          <Select.Option value="">
-                            Vui Lòng Chọn Màu
-                          </Select.Option>
-                          {dataColors?.map((item: any, index: number) => (
-                            <Select.Option key={index} value={item.name}>
-                              {item.name}
+                        <Form.Item
+                          {...restField}
+                          label={`Color #${name}`}
+                          name={[name, "color"]}
+                          fieldKey={`[${fieldKey}, "color"]`}
+                          style={{
+                            marginBottom: 0,
+                          }}
+                          rules={[
+                            { required: true, message: "* Color is required" },
+                          ]}
+                        >
+                          <Select size={SIZEFORM} placeholder="Color">
+                            <Select.Option value="">
+                              Vui Lòng Chọn Màu
                             </Select.Option>
-                          ))}
-                        </Select>
-                      </Form.Item>
+                            {dataColors?.map((item: any, index: number) => (
+                              <Select.Option key={index} value={item.name}>
+                                {item.name}
+                              </Select.Option>
+                            ))}
+                          </Select>
+                        </Form.Item>
 
-                      <Form.Item
-                        {...restField}
-                        label={`Quantity #${name}`}
-                        name={[name, "quantity"]}
-                        fieldKey={`[${fieldKey}, "quantity"]`}
-                        style={{
-                          marginBottom: 0,
-                        }}
-                        rules={[
-                          { required: true, message: "* Quantity is required" },
-                        ]}
-                      >
-                        <Input size={SIZEFORM} placeholder="Quantity" />
-                      </Form.Item>
+                        <Form.Item
+                          {...restField}
+                          label={`Quantity #${name}`}
+                          name={[name, "quantity"]}
+                          fieldKey={`[${fieldKey}, "quantity"]`}
+                          style={{
+                            marginBottom: 0,
+                          }}
+                          rules={[
+                            {
+                              required: true,
+                              message: "* Quantity is required",
+                            },
+                          ]}
+                        >
+                          <Input size={SIZEFORM} placeholder="Quantity" />
+                        </Form.Item>
 
-                      <MinusCircleOutlined
-                        style={{
-                          padding: "10px",
-                        }}
-                        onClick={() => {
-                          remove(name);
-                        }}
-                      />
-                    </div>
-                  ))}
-                </div>
-                <Form.Item>
-                  <Button
-                    type="dashed"
-                    onClick={() => {
-                      add();
-                    }}
-                  >
-                    Add Product
-                  </Button>
-                </Form.Item>
-              </>
-            )}
-          </Form.List>
+                        <MinusCircleOutlined
+                          style={{
+                            padding: "10px",
+                          }}
+                          onClick={() => {
+                            remove(name);
+                          }}
+                        />
+                      </div>
+                    ))}
+                  </div>
+                  <Form.Item>
+                    <Button
+                      type="dashed"
+                      onClick={() => {
+                        add();
+                      }}
+                    >
+                      Add Product
+                    </Button>
+                  </Form.Item>
+                </>
+              )}
+            </Form.List>
 
-          <div className="grid gap-6 mb-4 md:grid-cols-2">
-            <Form.Item
-              label="Customer Name"
-              name="nameCustomer"
-              style={{
-                marginBottom: 0,
-              }}
-              rules={[
-                { required: true, message: "* Customer Name is required" },
-              ]}
-            >
-              <Input size={SIZEFORM} placeholder="Customer Name" />
-            </Form.Item>
-
-            <Form.Item
-              label="Payment Method"
-              name="paymentID"
-              style={{
-                marginBottom: 0,
-              }}
-              rules={[{ required: true, message: "* Payments is required" }]}
-            >
-              <Select size={SIZEFORM} placeholder="Phương Thức Thanh Toán">
-                <Select.Option value="">
-                  Vui Lòng Phương Thức Thanh Toán
-                </Select.Option>
-                {isPayment?.map((item: any, index: number) => (
-                  <Select.Option key={index} value={item._id}>
-                    {item.namePayment}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Form.Item>
-
-            {user && user?.role === "ADMIN" && user?.verify == 1 ? (
+            <div className="grid gap-6 mb-4 md:grid-cols-2">
               <Form.Item
-                label="Nhân Viên"
-                name="assignedToID"
+                label="Customer Name"
+                name="nameCustomer"
                 style={{
                   marginBottom: 0,
                 }}
-                rules={[{ required: true, message: "* Assigned is required" }]}
+                rules={[
+                  { required: true, message: "* Customer Name is required" },
+                ]}
               >
-                <Select
-                  size={SIZEFORM}
-                  placeholder="Nhân Viên Quản Lí Đơn Hàng"
-                >
+                <Input size={SIZEFORM} placeholder="Customer Name" />
+              </Form.Item>
+
+              <Form.Item
+                label="Payment Method"
+                name="paymentID"
+                style={{
+                  marginBottom: 0,
+                }}
+                rules={[{ required: true, message: "* Payments is required" }]}
+              >
+                <Select size={SIZEFORM} placeholder="Phương Thức Thanh Toán">
                   <Select.Option value="">
-                    Vui Lòng Chọn Nhân Viên
+                    Vui Lòng Phương Thức Thanh Toán
                   </Select.Option>
-                  {isUser
-                    ?.filter(
-                      (item: any) =>
-                        item.role === "EMPLOYEE" || item.role === "ADMIN"
-                    )
-                    .map((item: any) => (
-                      <Select.Option key={item._id} value={item._id}>
-                        {item.fullname} -- ({item.role})
-                      </Select.Option>
-                    ))}
+                  {isPayment?.map((item: any, index: number) => (
+                    <Select.Option key={index} value={item._id}>
+                      {item.namePayment}
+                    </Select.Option>
+                  ))}
                 </Select>
               </Form.Item>
-            ) : user && user?.role === "EMPLOYEE" && user?.verify == 1 ? (
-              <Form.Item
-                label="Assigned To"
-                name="assignedToID"
-                style={{
-                  marginBottom: 0,
-                }}
-                initialValue={user?._id}
-              >
-                <Input disabled />
-              </Form.Item>
-            ) : (
-              <Empty />
-            )}
-          </div>
 
-          <div className="py-5">
-            <button
-              type="submit"
-              className=" text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-            >
-              Save
-            </button>
-          </div>
-        </Form>
+              {user && user?.role === "ADMIN" && user?.verify == 1 ? (
+                <Form.Item
+                  label="Nhân Viên"
+                  name="assignedToID"
+                  style={{
+                    marginBottom: 0,
+                  }}
+                  rules={[
+                    { required: true, message: "* Assigned is required" },
+                  ]}
+                >
+                  <Select
+                    size={SIZEFORM}
+                    placeholder="Nhân Viên Quản Lí Đơn Hàng"
+                  >
+                    <Select.Option value="">
+                      Vui Lòng Chọn Nhân Viên
+                    </Select.Option>
+                    {isUser
+                      ?.filter(
+                        (item: any) =>
+                          item.role === "EMPLOYEE" || item.role === "ADMIN"
+                      )
+                      .map((item: any) => (
+                        <Select.Option key={item._id} value={item._id}>
+                          {item.fullname} -- ({item.role})
+                        </Select.Option>
+                      ))}
+                  </Select>
+                </Form.Item>
+              ) : user && user?.role === "EMPLOYEE" && user?.verify == 1 ? (
+                <Form.Item
+                  label="Assigned To"
+                  name="assignedToID"
+                  style={{
+                    marginBottom: 0,
+                  }}
+                  initialValue={user?._id}
+                >
+                  <Input disabled />
+                </Form.Item>
+              ) : (
+                <Empty />
+              )}
+            </div>
+
+            <div className="py-5">
+              <button
+                type="submit"
+                className=" text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+              >
+                Save
+              </button>
+            </div>
+          </Form>
+        )}
       </div>
     </Layout>
   );
